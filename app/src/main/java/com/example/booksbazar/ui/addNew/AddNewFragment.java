8197c3dde1fea976f5ef254c6cc949420b1ef636
Kenan Babic <kenanbc223@gmail.com>
2025-04-29 22:27:02 +0100

    Created Add New Book Fragment

--- app/src/main/java/com/example/booksbazar/ui/addNew/AddNewFragment.java
+++ app/src/main/java/com/example/booksbazar/ui/addNew/AddNewFragment.java
@@ -7,16 +7,27 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.booksbazar.R;
+
+import org.w3c.dom.Text;
 
 public class AddNewFragment extends Fragment {
 
     private AddNewViewModel mViewModel;
+    private TextView newTitleInput;
+    private TextView newAuthorInput;
+    private TextView newGenreInput;
+    private EditText newPriceInput;
+    private TextView newImageInput;
+
 
     public static AddNewFragment newInstance() {
         return new AddNewFragment();
@@ -25,14 +36,16 @@ public class AddNewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_new, container, false);
+        View root = inflater.inflate(R.layout.fragment_add_new, container, false);
+
+        newTitleInput = root.findViewById(R.id.newTitleInput);
+        newAuthorInput = root.findViewById(R.id.newAuthorInput);
+        newGenreInput = root.findViewById(R.id.newGenreInput);
+        newPriceInput = root.findViewById(R.id.newPriceInput);
+        newImageInput = root.findViewById(R.id.newImageInput);
+
+        return root;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AddNewViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }
