46e57bd86a5a8a870acccbe11dbe4bfcd95f39c3
Kenan Babic <kenanbc223@gmail.com>
2025-04-30 08:40:17 +0100

    Layout  and navigation

--- app/src/main/java/com/example/booksbazar/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/booksbazar/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.booksbazar.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,29 +10,65 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.booksbazar.Book;
+import com.example.booksbazar.BookAdapter;
+import com.example.booksbazar.R;
 import com.example.booksbazar.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private RecyclerView recyclerView;
+    private BookAdapter bookAdapter;
+    private List<Book> bookList;
+    private FirebaseFirestore db;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    public HomeFragment() {
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = root.findViewById(R.id.recyclerViewBooks);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        db = FirebaseFirestore.getInstance();
+        bookList = new ArrayList<>();
+        bookAdapter = new BookAdapter(getContext(), bookList, R.id.action_nav_home_to_bookDetailFragment);
+        recyclerView.setAdapter(bookAdapter);
+
+        loadBooks();
+
+        return root;
+    }
+
+    private void loadBooks() {
+        db.collection("books")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Book book = document.toObject(Book.class);
+                            book.setBookId(document.getId());
+                            bookList.add(book);
+                        }
+
+                        bookAdapter.notifyDataSetChanged();
+                    } else {
+                        Log.w("Firestore", "Error getting documents.", task.getException());
+                    }
+                });
     }
 }
